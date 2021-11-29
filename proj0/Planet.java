ab859501653a995e316c4baf55468b07076ff776
EthanChang2021 <ethan1027.chang@gmail.com>
2021-11-29 00:30:28 -0500

    proj0 have finished

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Planet {
-	public static final double G = 6.67e-11;
+	private static final double G = 6.67e-11;
 
 	public double xxPos;		//current x position
 	public double yyPos;		//current y position
